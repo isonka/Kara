aa3dcf73f5c2be2f82fa7a1c5fd46ec540f1203e
isonka <isonka1@hotmail.com>
2025-09-23 21:04:58 +0200

    Fix MongoDB connection issues

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 	useUnifiedTopology: true,
 	serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
 	socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-	bufferMaxEntries: 0, // Disable mongoose buffering
-	bufferCommands: false, // Disable mongoose buffering
+	maxPoolSize: 10, // Maintain up to 10 socket connections
 })
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
